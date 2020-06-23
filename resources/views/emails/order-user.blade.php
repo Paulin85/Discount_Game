5b7ca85e3e6a911733c9a58b363bc3ac07b7e1b4
Paulin <paulinsirot85@gmail.com>
2020-06-23 13:17:43 +0200

    heroku MAIL V3

--- resources/views/emails/order-user.blade.php
+++ resources/views/emails/order-user.blade.php
@@ -1,7 +1,11 @@
 @component('mail::message')
 # Merci à vous d'avoir eu confiance en nous !
 
+@foreach (unserialize($order->products) as $product)
 
+Voici le code d'activation pour {{ $product[0] }} : {{ $product[4] }}
+
+@endforeach
 
 Cliquez-ci dessous pour récupérer votre facture.
 
