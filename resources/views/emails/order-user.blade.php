df2b13a0409b2478f789529c105112a9728bb640
Paulin <paulinsirot85@gmail.com>
2020-06-23 13:05:56 +0200

    heroku MAIL V2

--- resources/views/emails/order-user.blade.php
+++ resources/views/emails/order-user.blade.php
@@ -1,15 +1,13 @@
 @component('mail::message')
 # Merci à vous d'avoir eu confiance en nous !
 
-@foreach (unserialize($order->products) as $product)
 
-Voici le code d'activation pour {{ $product[0] }} : {{ $product[4] }}
-
-@endforeach
 
 Cliquez-ci dessous pour récupérer votre facture.
 
-
+@component('mail::button', ['url' => 'http://discount-game.herokuapp.com/home', 'color' => 'success'])
+Télécharger la facture (pdf).
+@endcomponent
 
 Merci, l'équipe Discount Gaming.<br>
 @endcomponent
